b078da69aff4ef28141e2fc19c58813f079ab189
atharva-u-01 <70993547+atharva-u-01@users.noreply.github.com>
2021-10-24 20:26:27 +0530

    - removed .

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -8,31 +8,32 @@ export default function SignIn(props: {
 }) {
   return (
     <div className="signup-login-route">
-      ̥
-      <header>
-        {/* <!-- <img src="./images/logo.jpg" alt=""> */}
-        {/* <h2 id="projectName">Project Approval System</h2> */}
-        {/* <h3 id="tagling">Get approved right now</h3> --> */}
-        <img src={headerImg} alt="" />
-      </header>
-      <div>Please enter your login and password</div>
-      <form action="" method="post">
-        <h1>Log in</h1>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="text" id="password" />
-        </div>
-        <a href="/forgotPassword.html" target="_blank">
-          Forgot password?
-        </a>
-        <br />
-        <button type="submit">Log in</button>
-        <br />
-      </form>
+      <main>
+        <header>
+          {/* <!-- <img src="./images/logo.jpg" alt=""> */}
+          {/* <h2 id="projectName">Project Approval System</h2> */}
+          {/* <h3 id="tagling">Get approved right now</h3> --> */}
+          <img src={headerImg} alt="" />
+        </header>
+        <div>Please enter your login and password</div>
+        <form action="" method="post">
+          <h1>Log in</h1>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" />
+          </div>
+          <a href="/forgotPassword.html" target="_blank">
+            Forgot password?
+          </a>
+          <br />
+          <button type="submit">Log in</button>
+          <br />
+        </form>
+      </main>
     </div>
   );
 }
